5e96812c24d952498719481314007c00b62d1a51
ISvalina09 <ivan.svalina@endava.com>
2023-10-31 17:31:20 +0100

    Cherry picked

--- zadatak1.py
+++ zadatak1.py
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 print("Ovo je moje ime: Ivan Svalina")
 print("Moj broj indeksa: 123456789")
+=======
+print("Ovo je moje ime: Ivan Svalina")
+>>>>>>> 0fdb516 (First commit on a new branch)
