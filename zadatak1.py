3a93966cf5ce34593f1bc43e59fb0465ddaec7c9
ISvalina09 <ivan.svalina@endava.com>
2023-10-31 17:31:20 +0100

    Cherry picked from development branch before adding new line

--- zadatak1.py
+++ zadatak1.py
@@ -1,6 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Ovo je moje ime: Ivan Svalina")
 print("Moj broj indeksa: 123456789")
+=======
+print("Ovo je moje ime: Ivan Svalina")
+>>>>>>> 0fdb516 (First commit on a new branch)
 =======
 print("Ovo je moje ime: Ivan Svalina")
 >>>>>>> 0fdb516 (First commit on a new branch)
